7486098e5392ba31702b732bc9bd668193398dd6
Jonathan Mills <jon@badger.shoes>
2021-05-18 10:41:54 +0800

    Move code to init.py

--- setup.py
+++ setup.py
@@ -13,7 +13,10 @@ setuptools.setup  (name                          =  thisName
                   ,long_description              =  Path("README.md").read_text()
                   ,long_description_content_type =  "text/markdown"
                   ,url                           =  baseURL
-                  ,project_urls                  =  {"Bug Tracker": f"{baseURL}/issues"}
+                  ,project_urls                  =  {"Bug Tracker"      :   f"{baseURL}/issues"
+                                                    ,"Source"           :   baseURL
+                                                    ,"Documentation"    :   f"{baseURL}/wiki"
+                                                    }
                   ,classifiers                   =  ["Programming Language :: Python :: 3"
                                                     ,"License :: OSI Approved :: MIT License"
                                                     ,"Operating System :: OS Independent"
